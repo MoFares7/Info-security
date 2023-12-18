c1d166a952eba11d945e22c6264b1f7a1195ebef
MoFares7 <faresdabbas1@gmail.com>
2023-12-18 15:18:32 +0300

    initial project

--- src/routes/RouteApp.jsx
+++ src/routes/RouteApp.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from '../pages/HomePage/HomePage';
-import LoginPage from '../pages/AuthPage/LoginPage';
-import SignUpPage from '../pages/AuthPage/SignUpPage';
-
 
 const RouteApp = () => {
         return (
                 <Routes>
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/signUp" element={<SignUpPage />} />
-                        <Route element={<SidebarLayout />}>
-                                <Route index element={<HomePage />} />
-                                <Route path="/public" element={<PublicGroupPage />} />
-                                <Route path="/privet" element={<PrivetGroupPage />} />
-                                <Route path="/files/:groupId" element={<FilesPage />} />
-                                <Route path="/login" element={<LoginPage />} />
-                        </Route>
+                        //! when create login and signUp page only Uncomment this statments and import
+                        {/* <Route path="/login" element={<LoginPage />} /> */}
+                        {/* <Route path="/signUp" element={<SignUpPage />} /> */}
+
+                        {/* <Route path="/home" element={<HomePage />} /> */}
                 </Routes>
 
         )
